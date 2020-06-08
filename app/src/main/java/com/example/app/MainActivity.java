38c52877034027a8aa76cc6de79308e3fdd2b3ac
xiedaifeng <1091312423@qq.com>
2020-06-08 16:29:08 +0800

    test

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.app;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_click)
+    Button btnClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_click)
+    public void onViewClicked() {
+        Log.e("error","click");
+    }
+}
